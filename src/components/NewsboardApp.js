aa30cb4027ec2cdf6c224c476eebb3d7badf8418
tikkuk <karantikku7@gmail.com>
2018-10-12 05:09:37 +0530

    fetch remote api data -fetching and printing remote api data from hacker news api.

--- src/components/NewsboardApp.js
+++ src/components/NewsboardApp.js
@@ -3,6 +3,18 @@ import Header from './Header';
 import NewsList from './NewsList';
 
 export class NewsboardApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            news: []
+        };
+    }
+    componentDidMount() {
+        fetch('http://hn.algolia.com/api/v1/items/6')
+        .then(results => {
+            console.log(results.json());
+        })
+    }
     render() {
         return(
             <div>
