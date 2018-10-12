f5151234dd36fd08df0f6a7524fae60134270030
karantikku <karantikku7@gmail.com>
2018-10-12 17:41:05 +0530

    fetched data from hn api.
    fetched and render newslist components from hn api.

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import NewsItem from './NewsItem'; 
+import React from "react";
+import NewsItem from "./NewsItem";
 
-const NewsList = (props) => (
-    <div>
-        NewsList Here
-        <NewsItem />
-    </div>
-
+const NewsList = props => (
+  <div>
+    {props.news.length === 0 && <p>No Stories to show</p>}
+    {props.news.map(newsItem => (
+      <NewsItem
+        key={newsItem.title}
+        title={newsItem.title}
+        author={newsItem.author}
+        points={newsItem.points}
+        url={newsItem.url}
+      />
+    ))}
+  </div>
 );
 
 export default NewsList;
--- src/components/NewsboardApp.js
+++ src/components/NewsboardApp.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import Header from './Header';
-import NewsList from './NewsList';
+import React from "react";
+import fetch from "node-fetch";
+import Header from "./Header";
+import NewsList from "./NewsList";
 
 export class NewsboardApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            news: []
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: []
+    };
+  }
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    fetch("https://hn.algolia.com/api/v1/search?tags=front_page")
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Something went wrong");
+        }
+      })
+      .then(data => {
+        this.setState(prevState => ({
+          news: data.hits,
+          isLoading: false
+        }));
+      })
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+  render() {
+    const isLoading = this.state.isLoading;
+    if (isLoading) {
+      return <p>Loading ...</p>;
     }
-    componentDidMount() {
-        fetch('http://hn.algolia.com/api/v1/items/6')
-        .then(results => {
-            console.log(results.json());
-        })
+    const error = this.state.error;
+    if (error) {
+      return <p>{error.message}</p>;
     }
-    render() {
-        return(
-            <div>
-                <Header />
-                <NewsList />
-            </div>   
-        );
-    }
+    return (
+      <div>
+        <Header />
+        <NewsList news={this.state.news} />
+      </div>
+    );
+  }
 }
