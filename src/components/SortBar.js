179e7a1534c713431ef7df7be44dd6dbb02ac78d
tikkuk <karantikku7@gmail.com>
2018-10-16 23:35:25 +0530

    fixed Sort button

--- src/components/SortBar.js
+++ src/components/SortBar.js
@@ -1,17 +1,12 @@
 import React from "react";
 import NativeSelect from "@material-ui/core/NativeSelect";
 class SortBar extends React.Component {
-  onSort(event) {
-    if (event.target.value) {
-      console.log(props);
-    }
-  }
 
   render() {
     return (
       <div>
         Sort By :
-        <NativeSelect onChange={this.onSort}>
+        <NativeSelect onChange={event => this.props.onSort(event.target.value)}>
           <option value={"Date"}>Date</option>
           <option value={"Popularity"}>Popularity</option>
         </NativeSelect>
